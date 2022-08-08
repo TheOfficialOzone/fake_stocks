41825ad89609f138267d628acdd4d8cda5da1173
Owen Zonneveld <owenzonneveld@gmail.com>
2022-08-08 12:10:57 -0500

    Removed unused code

--- src/data/data_saving.rs
+++ src/data/data_saving.rs
@@ -2,7 +2,7 @@
 
 use std::io::prelude::*;
 use std::fs::File;
-use std::io::Write;
+//use std::io::Write;
 
 
 pub trait SaveData {
@@ -30,25 +30,25 @@ pub fn read_from_file(filename : &str) -> Result<String, String> {
     Ok(contents)
 }
 
-/// Saves data to a file
-pub fn save_to_file(filename : &str, data : &String) -> Result<(), String> {
-    //Creates the file
-    let file_result = File::create(filename);
+// Saves data to a file
+// pub fn save_to_file(filename : &str, data : &String) -> Result<(), String> {
+//     //Creates the file
+//     let file_result = File::create(filename);
     
-    //Opens the file
-    let mut file;
-    match file_result {
-        Ok(new_file) => file = new_file,
-        Err(error) => return Err(error.to_string()),
-    }
-    //Writes all the data to the file
-    let write_result = file.write(data.as_bytes());
+//     //Opens the file
+//     let mut file;
+//     match file_result {
+//         Ok(new_file) => file = new_file,
+//         Err(error) => return Err(error.to_string()),
+//     }
+//     //Writes all the data to the file
+//     let write_result = file.write(data.as_bytes());
 
-    //Checks that the write result is fully valid
-    match write_result {
-        Ok(value) => (if value == 0 {return Err(String::from("Unknown error writing to file"))}),
-        Err(error) => return Err(error.to_string()),
-    }
+//     //Checks that the write result is fully valid
+//     match write_result {
+//         Ok(value) => (if value == 0 {return Err(String::from("Unknown error writing to file"))}),
+//         Err(error) => return Err(error.to_string()),
+//     }
 
-    Ok(())
-}
+//     Ok(())
+// }
